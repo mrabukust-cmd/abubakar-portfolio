27f3ec289efe5b4b15879f6828a24d38e6a2b544
mrabukust-cmd <mrabukust@gmail.com>
2026-08-10 22:17:16 +0500

    remove unused productionUrl field from socialLinks

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -2,8 +2,7 @@ export const socialLinks = {
   github: "https://github.com/mrabukust-cmd",
   linkedin: "https://www.linkedin.com/in/abu-bakar-siddique-a82747425/",
   email: "abubakar1siddique2@gmail.com",
-  mailto: "mailto:abubakar1siddique2@gmail.com",
-  productionUrl: "https://abubakar-siddique.vercel.app"
+  mailto: "mailto:abubakar1siddique2@gmail.com"
 };
 
 export const formatSocialDisplay = (url) => {
